244c3b2bf8b0ba1f18892955434ca48b771b090c
Bazavod <eugeny.bazavod@list.ru>
2025-05-05 20:08:43 +0300

    solve lint mistakes

--- src/server.py
+++ src/server.py
@@ -1,7 +1,6 @@
 # Импорт встроенной библиотеки для работы веб-сервера
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-
 # Для начала определим настройки запуска
 hostName = "localhost"  # Адрес для доступа по сети
 serverPort = 8080  # Порт для доступа по сети
